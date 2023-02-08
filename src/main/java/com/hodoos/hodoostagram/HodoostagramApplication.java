dca97689bff4be769dd535303500459bc1edf460
hodoos <motoboxx@naver.com>
2023-02-08 19:29:48 +0900

    [이종호] hodoostagram project 최초 파일 추가

---
+++ src/main/java/com/hodoos/hodoostagram/HodoostagramApplication.java
@@ -0,0 +1,13 @@
+package com.hodoos.hodoostagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodoostagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodoostagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hodoos/hodoostagram/HodoostagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodoos.hodoostagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodoostagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
